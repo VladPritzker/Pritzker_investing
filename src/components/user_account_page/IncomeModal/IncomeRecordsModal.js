d305b186721c2fa04aab6cfadb5be82438aca1c9
VladPritzker <94125540+VladPritzker@users.noreply.github.com>
2024-08-07 13:10:39 -0400

    fix

--- src/components/user_account_page/IncomeModal/IncomeRecordsModal.js
+++ src/components/user_account_page/IncomeModal/IncomeRecordsModal.js
@@ -17,6 +17,23 @@ function IncomeRecordsModal({ user, onClose }) {
     const [showAddRecordModal, setShowAddRecordModal] = useState(false);
     const [showChartModal, setShowChartModal] = useState(false);
 
+    function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    
+
     useEffect(() => {
         const fetchIncomeRecords = async () => {
             try {
@@ -92,8 +109,13 @@ function IncomeRecordsModal({ user, onClose }) {
 
     const handleDeleteConfirm = async () => {
         try {
+            const csrfToken = getCookie('csrftoken'); // Implement getCookie function to fetch CSRF token from cookies
             const response = await fetch(`${apiUrl}/users/${user.id}/income_records/${recordToDelete.id}/`, {
                 method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': csrfToken,  // Include CSRF token
+                },
             });
             if (response.ok) {
                 setIncomeRecords(incomeRecords.filter(record => record.id !== recordToDelete.id));
@@ -107,6 +129,7 @@ function IncomeRecordsModal({ user, onClose }) {
             console.error('Error deleting income record:', error);
         }
     };
+    
 
     const handleDeleteCancel = () => {
         setShowDeleteModal(false);
